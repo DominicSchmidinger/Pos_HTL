c7ea155551f03a723e4b24429b1746890790fb89
DominicSchmidinger <kas240614@spengergasse.at>
2026-03-15 12:13:24 +0100

    werkstoffprobe hinzugefügt

---
+++ Coding/Werkstoffprobe/src/main/java/MetallProbe.java
@@ -0,0 +1,55 @@
+import at.spengergasse.Werkstoffprobe;
+
+public class MetallProbe extends Werkstoffprobe {
+    private double zugfestigkeit;
+    private double korrisonsWert;
+
+    public MetallProbe(String id, double dichte, String bezeichnung, double masse) {
+        super(id, dichte, bezeichnung, masse);
+        setZugfestigkeit(zugfestigkeit);
+        setKorrisonsWert(korrisonsWert);
+    }
+
+    public double getZugfestigkeit() {
+        return zugfestigkeit;
+    }
+
+    public void setZugfestigkeit(double zugfestigkeit) {
+        if (zugfestigkeit > 0){
+            throw new IllegalArgumentException("zugfestigkeit kann nicht negativ sein");
+        }
+        this.zugfestigkeit = zugfestigkeit;
+    }
+
+    public double getKorrisonsWert() {
+        return korrisonsWert;
+    }
+
+    public void setKorrisonsWert(double korrisonsWert) {
+        if (korrisonsWert > 0 || korrisonsWert <  100){
+            throw new IllegalArgumentException("korrisonswert muss zwischen 0-100 sein (laut prof halt ka was das is)");
+        }
+        this.korrisonsWert = korrisonsWert;
+    }
+
+    public double berechneQualitaetsIndex(){
+        double qualitaetsindex = 0;
+        qualitaetsindex = zugfestigkeit/ (korrisonsWert +1);
+        return qualitaetsindex;
+    }
+
+    public double berechneVolumen(){
+        double volumen = 0;
+        volumen = getDichte()/getDichte();
+        return volumen;
+    }
+
+
+    @Override
+    public String toString() {
+        return super.toString() +"MetallProbe{" +
+                "zugfestigkeit=" + zugfestigkeit +
+                ", korrisonsWert=" + korrisonsWert +
+                "} ";
+    }
+}
--- Coding/Werkstoffprobe/src/main/java/at/spengergasse/Werkstoffprobe.java
+++ Coding/Werkstoffprobe/src/main/java/at/spengergasse/Werkstoffprobe.java
@@ -1,5 +1,73 @@
 package at.spengergasse;
 
-public abstract Werkstoffprobe {
+public abstract class Werkstoffprobe {
+    private String id;
+    private String bezeichnung;
+    private double dichte;
+    private double masse;
 
+    public Werkstoffprobe(String id, double dichte, String bezeichnung, double masse) {
+        this.id = id;
+        this.dichte = dichte;
+        this.bezeichnung = bezeichnung;
+        this.masse = masse;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        if (id == null || id.isBlank()) {
+            throw new IllegalArgumentException("Id darf nicht null oder leer sein");
+        }
+        this.id = id;
+    }
+
+    public String getBezeichnung() {
+        return bezeichnung;
+    }
+
+    public void setBezeichnung(String bezeichnung) {
+        if (bezeichnung == null || bezeichnung.isEmpty()){
+            throw new IllegalArgumentException("bezeichnung darf nicht null oder leer sein");
+        }
+        this.bezeichnung = bezeichnung;
+    }
+
+    public double getDichte() {
+        return dichte;
+    }
+
+    public void setDichte(double dichte) {
+        if (dichte == 0){
+            throw new IllegalArgumentException("jede Probe hat eine dichte");
+        }
+        this.dichte = dichte;
+    }
+
+    public double getMasse() {
+        return masse;
+    }
+
+    public void setMasse(double masse) {
+        if (masse == 0){
+            throw new IllegalArgumentException("jedes Objekt hat eine masse");
+        }
+        this.masse = masse;
+    }
+
+    public abstract double berechneVolumen();
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Werkstoffprobe{");
+        sb.append("id='").append(id).append('\'');
+        sb.append(", bezeichnung='").append(bezeichnung).append('\'');
+        sb.append(", dichte=").append(dichte);
+        sb.append(", masse=").append(masse);
+        sb.append('}');
+        return sb.toString();
+    }
 }
+
