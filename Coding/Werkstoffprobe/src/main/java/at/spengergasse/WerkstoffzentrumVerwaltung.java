2920575cd1826dca50e33204a60727323b96e935
DominicSchmidinger <kas240614@spengergasse.at>
2026-03-16 23:35:14 +0100

    Code fertig

--- Coding/Werkstoffprobe/src/main/java/at/spengergasse/WerkstoffzentrumVerwaltung.java
+++ Coding/Werkstoffprobe/src/main/java/at/spengergasse/WerkstoffzentrumVerwaltung.java
@@ -1,37 +1,149 @@
 package at.spengergasse;
 
+import java.util.Comparator;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 
 public class WerkstoffzentrumVerwaltung {
 
-    private List<MetallProbe> metallerProbe;
+    private List<Werkstoffprobe> Werkstoffprobe;
 
     public WerkstoffzentrumVerwaltung() {
-        metallerProbe = new LinkedList<MetallProbe>();
+        Werkstoffprobe = new LinkedList<Werkstoffprobe>();
     }
 
-    public boolean probenaufnehmen(MetallProbe probe) {
-        if (probe == null || metallerProbe.contains(probe)) {
+    public boolean probenaufnehmen(Werkstoffprobe probe) {
+        if (probe == null || Werkstoffprobe.contains(probe)) {
             throw new IllegalArgumentException("Fehler bei der Validierung: " + probe);
         }
-        return metallerProbe.add(probe);
+        return Werkstoffprobe.add(probe);
 
     }
-public boolean removeProbe(MetallProbe probe){
-        Iterator<MetallProbe> iterator = metallerProbe.iterator();
+public boolean removeProbe(String id){
+        Iterator<Werkstoffprobe> iterator = Werkstoffprobe.iterator();
         while (iterator.hasNext()) {
-            if (iterator.next().getId().equals(probe.getId())) {
+            if (iterator.next().getId().equals(id)) {
                 iterator.remove();
             }
         }
         return false;
 }
-public boolean removeAllProbe(){
-        Iterator<MetallProbe> iterator = metallerProbe.iterator();
+public int removeAllProbe(String Name){
+        int glöschteProbe = 0;
+        Iterator<Werkstoffprobe> iterator = Werkstoffprobe.iterator();
         while (iterator.hasNext()){
-            
+            if (iterator.next().getClass().getSimpleName().equals(Name)){
+                glöschteProbe++;
+                iterator.remove();
+            }
         }
+        return glöschteProbe;
 }
+
+public int AllMitIndexUnter (double grenzwert){
+        int grenzwertzuhoch = 0;
+        Iterator<Werkstoffprobe> iter = Werkstoffprobe.iterator();
+        while (iter.hasNext()){
+            if (iter.next().berechneQualitaetsIndex() < grenzwert){
+                grenzwertzuhoch++;
+                iter.remove();
+            }
+        } return grenzwertzuhoch;
 }
+
+public Werkstoffprobe sucheProbe (String id){
+        for (Werkstoffprobe wp : Werkstoffprobe){
+            if (wp.getId().contains(id)){
+                return wp;
+            }
+        } return null;
+}
+
+public int zaehleMetallproben(){
+   int anzahl = 0;
+    for (Werkstoffprobe wp : Werkstoffprobe) {
+        if (wp instanceof MetallProbe) {
+            anzahl++;
+        }
+    }return anzahl;
+    }
+
+    public int zaehlePolymerproben(){
+        int anzahl = 0;
+        for (Werkstoffprobe wp : Werkstoffprobe) {
+            if (wp instanceof PolymerProbe)  {
+                anzahl++;
+            }
+        }return anzahl;
+    }
+    public int zeahleKeramikProben(){
+        int anzahl = 0;
+        for (Werkstoffprobe wp : Werkstoffprobe) {
+            if (wp instanceof KeramikProbe)  {
+                anzahl++;
+            }
+        }return anzahl;
+    }
+
+    public double gesQuliteatMetall() {
+        double summe = 0;
+        for (Werkstoffprobe wp : Werkstoffprobe) {
+            if (wp instanceof MetallProbe)
+            summe += wp.berechneQualitaetsIndex();
+        }
+        return summe;
+    }
+
+    public double gesQuliteatKeramik() {
+        double summe = 0;
+        for (Werkstoffprobe wp : Werkstoffprobe) {
+            if (wp instanceof KeramikProbe)
+                summe += wp.berechneQualitaetsIndex();
+        }
+        return summe;
+    }
+
+
+    public double gesQuliteatPolymer() {
+        double summe = 0;
+        for (Werkstoffprobe wp : Werkstoffprobe) {
+            if (wp instanceof KeramikProbe)
+                summe += wp.berechneQualitaetsIndex();
+        }
+        return summe;
+    }
+
+    public double durchschnittMetall(){
+        double durchschnitt = 0;
+        durchschnitt = gesQuliteatMetall() / zaehleMetallproben();
+        return durchschnitt;
+    }
+    public double durchschnittKeramik() {
+        double durchschnitt = 0;
+        durchschnitt = gesQuliteatKeramik() / zeahleKeramikProben();
+        return durchschnitt;
+    }
+
+    public double durchschnittPolymer() {
+        double durchschnitt = 0;
+        durchschnitt = gesQuliteatPolymer() / zaehlePolymerproben();
+        return durchschnitt;
+    }
+
+
+    public void druckeStatistik(){
+    System.out.println("----Werkstoffzentrum Statistik----");
+    System.out.println("Gesamtzahl Metallproben: " + zaehleMetallproben());
+    System.out.println("Gesamtzahl PolymerProben: " + zaehlePolymerproben());
+    System.out.println("Gesamtzahl KeramikProben: " + zeahleKeramikProben());
+        System.out.println("________________________________________________");
+    }
+    public void Standartsort(){
+        Werkstoffprobe.sort(null);
+    }
+    public void sotierencoperator(){
+        Werkstoffprobe.sort(new WerkstoffQualitaetsIndexComparator());
+    }
+}
+
